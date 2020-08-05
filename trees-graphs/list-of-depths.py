90e8dff8ff36e2163a58dfdb8d55cf9ca9d57419
Dom Eccleston <domeccleston@gmail.com>
2020-08-06 00:28:18 +0100

    latest

---
+++ trees-graphs/list-of-depths.py
@@ -0,0 +1,21 @@
+""" 
+List of Depths: Given a binary tree, design an algorithm which creates a linked list of all the nodes
+at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+"""
+
+class TreeNode:
+  pass
+
+class ListNode:
+  pass
+
+
+def list_of_depths(node: TreeNode) -> ListNode[]:
+  """ 
+  pseudo:
+    - initialize listlist to hold linked lists
+    - traverse the binary tree breadth-first, keeping track of level
+    - for each level, initialize the first node encountered as the head of a linked list and append the others
+    - push the linked list the listlist
+    - finally return the listlist
+  """
